edcf4129162e0bd9ee794bd529762aff7e8392de
JNickBaughan <JNickBaughan@gmail.com>
2021-01-31 14:04:47 -0500

    add scanner to print incoming connection text

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -8,19 +9,46 @@ import (
 )
 
 func main () {
-	tcpServer, err := net.Listen("tcp", ":8080")
+	//tcpServer is a Listener interface
+	/*
+		it has 3 methods
+		// accepts waits for and returns the next connection to the listener
+		accept
+		// closes the listener
+		close
+		// returns the listeners network address
+		addr
+	*/
+	tcpListener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Panic(err)
 	}
-	defer tcpServer.Close()
+	defer tcpListener.Close()
 
 	for {
-		connection, err := tcpServer.Accept();
+		// calling accept returns a connection
+		/*
+			connect is interface with both Read and Write methods
+		*/
+		connection, err := tcpListener.Accept();
 		if err != nil {
 			log.Panicln(err)
 		}
-		io.WriteString(connection, "\nHello GoLang\n")
-		fmt.Println("got a request")
-		connection.Close()
+		go handleRequest(connection)
+		 io.WriteString(connection, "\nHello GoLang\n")
+		 fmt.Println("got a request")
+		//connection.Close()
+
+
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	// hitting this on a browser will display the http request
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
 }
